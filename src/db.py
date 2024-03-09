0395d56753387c6975b5a90a1d7b3d03d64d6911
Huajie Zhong <hz642@cornell.edu>
2024-03-09 14:19:54 -0500

    Create Shopping_item model

--- src/db.py
+++ src/db.py
@@ -8,19 +8,20 @@ assoc_posts_users_actions = db.Table(
     db.Column("action_id", db.Integer, db.ForeignKey("action.id"))
 )
 
+
 class User(db.Model):
     """
     User Model
     """
 
     __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    points = db.Column(db.Integer, nullable = False, default = 0)
-    username = db.Column(db.String, nullable = False)
-    password = db.Column(db.String, nullable = False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    points = db.Column(db.Integer, nullable=False, default=0)
+    username = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
     spots = db.relationship("Spot")
-    actions = db.relationship("Action", secondary = assoc_posts_users_actions, back_populates = "users")
-    
+    actions = db.relationship(
+        "Action", secondary=assoc_posts_users_actions, back_populates="users")
 
     def __init__(self, **kwargs):
         """
@@ -40,8 +41,7 @@ class User(db.Model):
             "points": self.points,
             "actions": [action.simple_serialize() for action in self.actions]
         }
-    
-    
+
     def simple_serialize(self):
         """
         Serialize a user object without posts field
@@ -50,18 +50,19 @@ class User(db.Model):
             "id": self.id,
             "username": self.username
         }
-    
+
+
 class Park(db.Model):
     """
     Park Model
     """
 
     __tablename__ = "park"
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    name = db.Column(db.String, nullable = False)
-    longtitute = db.Column(db.Float, nullable = False)
-    latitude = db.Column(db.Float, nullable = False)
-    spots = db.relationship("Spot", cascade = "delete")
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    longtitute = db.Column(db.Float, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    spots = db.relationship("Spot", cascade="delete")
 
     def serialize(self):
         """
@@ -74,7 +75,7 @@ class Park(db.Model):
             "latitude": self.latitude,
             "spots": [spot.serialize() for spot in self.spots]
         }
-    
+
     def simple_serialize(self):
         """
         Serialize a park object without spots field
@@ -86,19 +87,20 @@ class Park(db.Model):
             "latitude": self.latitude
         }
 
+
 class Spot(db.Model):
     """
     Spot Model
     """
 
     __tablename__ = "spot"
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    name = db.Column(db.String, nullable = False)
-    longtitute = db.Column(db.Float, nullable = False)
-    latitude = db.Column(db.Float, nullable = False)
-    park_id = db.Column(db.Integer, db.ForeignKey("park.id"), nullable = False)
-    park = db.relationship("Park", back_populates = "spots")
-    actions = db.relationship("Action", cascade = "delete")
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    longtitute = db.Column(db.Float, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    park_id = db.Column(db.Integer, db.ForeignKey("park.id"), nullable=False)
+    park = db.relationship("Park", back_populates="spots")
+    actions = db.relationship("Action", cascade="delete")
 
     def serialize(self):
         """
@@ -112,7 +114,7 @@ class Spot(db.Model):
             "park": self.park.simple_serialize(),
             "actions": [action.serialize() for action in self.actions]
         }
-    
+
     def simple_serialize(self):
         """
         Serialize a spot object without actions field
@@ -131,11 +133,13 @@ class Action(db.Model):
     """
 
     __tablename__ = "action"
-    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
-    title = db.Column(db.String, nullable = False)
-    description = db.Column(db.String, nullable = False)
-    spot_id = db.Column(db.Integer, db.ForeignKey("spot.id"), nullable = False)
-    users_id = db.relationship("User", secondary = assoc_posts_users_actions, back_populates = "actions")
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    spot_id = db.Column(db.Integer, db.ForeignKey("spot.id"), nullable=False)
+    users_id = db.relationship(
+        "User", secondary=assoc_posts_users_actions, back_populates="actions")
+    is_verified = db.Column(db.Boolean, nullable=False, default=False)
 
     def serialize(self):
         """
@@ -148,7 +152,7 @@ class Action(db.Model):
             "spot": self.spot.simple_serialize(),
             "users": [user.simple_serialize() for user in self.users]
         }
-    
+
     def simple_serialize(self):
         """
         Serialize an action object without spot field
@@ -157,4 +161,37 @@ class Action(db.Model):
             "id": self.id,
             "title": self.title,
             "description": self.description
+        }
+
+
+class Shopping_item(db.Model):
+    """
+    Shop Model
+    """
+    __tablename__ = "shopping_item"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False)
+
+    def init(self, **kwargs):
+        """
+        Initialize a shop object
+        """
+        self.name = kwargs.get("name", "")
+        self.price = kwargs.get("price", "")
+        self.description = kwargs.get("description", "")
+        self.image = kwargs.get("image", "")
+
+    def serialize(self):
+        """
+        Serialize a shop object
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "description": self.description,
+            "image": self.image
         }
